c911d8f855196d6da99edbededc9d10894cb8061
Ridwan Jalali <jalalirsh@hotmail.com>
2022-06-24 19:46:35 +0300

    login to backend

--- falcoeye.py
+++ falcoeye.py
@@ -1,15 +1,37 @@
 import os
 import logging
 from dotenv import load_dotenv
+import requests
+import logging 
+from falcoeye_kubernetes import FalcoServingKube
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
+# login to backend
+workflow_user = os.getenv("WORKFLOW_USER")
+workflow_password = os.getenv("WORKFLOW_PASSWORD")
+
+backend_kube = FalcoServingKube("falcoeye-backend")
+backend_server = backend_kube.get_service_address()
+URL = f"http://{backend_server}"
+ 
+payload =  {
+        "email": workflow_user.strip(),
+        "password": workflow_password.strip()
+}
+r = requests.post(f"{URL}/auth/login", json=payload)
+assert "access_token" in r.json()
+access_token = r.json()["access_token"]
+os.environ["JWT_KEY"] = access_token
 
 from app import create_app
-
-logging.info(os.getenv("FLASK_CONFIG"))
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
 
 if __name__ == "__main__":
